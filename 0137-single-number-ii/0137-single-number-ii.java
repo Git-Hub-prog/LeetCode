3e81e1bb755df10b61ee653ca72adde9ac9485d1
Faiyaj-Ansari <faiyajansari0472@gmail.com>
2026-07-28 22:40:16 +0530

    Time: 6 ms (43.19%), Space: 46.6 MB (17.04%) - LeetHub

---
+++ 0137-single-number-ii/0137-single-number-ii.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int singleNumber(int[] nums) {
+        HashMap<Integer,Integer> mp = new HashMap<>();
+        for(int num : nums){
+            mp.put(num,mp.getOrDefault(num,0) + 1);
+        }
+        for(int num : nums){
+            if(mp.get(num) == 1) return num;
+        }
+        return -1;
+    }
+}
