66175fba199fc84bc021e4538292667c46e3e425
Faiyaj-Ansari <faiyajansari0472@gmail.com>
2026-08-04 23:39:47 +0530

    Time: 0 ms (100%), Space: 42.8 MB (53.32%) - LeetHub

--- 0100-same-tree/0100-same-tree.java
+++ 0100-same-tree/0100-same-tree.java
@@ -29,12 +29,8 @@ class Solution {
     //     }
     // }
     public boolean isSameTree(TreeNode p, TreeNode q) {
-        // if(preorder(p) == preorder(q)) returun true;
-        // else
-        // return false;
         if(p == null && q == null) return true;
         if(p == null || q == null) return false;
-
         if(p.val != q.val) return false;
         return isSameTree(p.left,q.left) && isSameTree(p.right,q.right);
     }
