50691b9aea382ccfd66e3a2811c88d810873740f
Faiyaj-Ansari <faiyajansari0472@gmail.com>
2026-08-06 23:26:40 +0530

    Time: 14 ms (97.37%), Space: 69.8 MB (79.22%) - LeetHub

---
+++ 0236-lowest-common-ancestor-of-a-binary-tree/0236-lowest-common-ancestor-of-a-binary-tree.java
@@ -0,0 +1,43 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+
+        if (root == null || root == p || root == q)
+            return root;
+
+        TreeNode left = lowestCommonAncestor(root.left, p, q);
+        TreeNode right = lowestCommonAncestor(root.right, p, q);
+
+        if (left != null && right != null)
+            return root;
+
+        return left != null ? left : right;
+    }
+}
+
+
+// class Solution {
+//     public boolean contains(TreeNode root, TreeNode node){
+//         if(root == null) return false;
+//         if(root == node) return true;
+//         return contains(root.left,node) || contains(root.right,node);
+//     }
+//     public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+//         if(p == root || q == root) return root;
+//         if(p == q) return p;
+//         boolean leftp = contains(root.left,p);
+//         boolean rightq = contains(root.right,q);
+//         if((leftp && rightq) || (!leftp && !rightq)) return root;
+//         if(leftp && !rightq ) return lowestCommonAncestor(root.left,p,q);
+//         if(!leftp && rightq) return lowestCommonAncestor(root.right,p,q);
+//         return null; // here meaning of this nothing , just write this to remove error 
+//     }
+// }
