b77552f1237779fcd7b28d7a871693ec9f3d595c
Faiyaj-Ansari <faiyajansari0472@gmail.com>
2026-08-09 23:48:10 +0530

    Time: 0 ms (100%), Space: 43.2 MB (11.04%) - LeetHub

--- 0226-invert-binary-tree/0226-invert-binary-tree.java
+++ 0226-invert-binary-tree/0226-invert-binary-tree.java
@@ -15,24 +15,9 @@
  */
 class Solution {
     public TreeNode invertTree(TreeNode root) {
-        // if(root == null) return null;
-        // TreeNode lf = root.left;
-        // TreeNode rt = root.right;
-        // root.left = rt;
-        // root.right = lf;
-        // //if(root.left == null && root.right == null) return root;
-        // if(root.left == null) return null;
-        // if(root.right == null) return null;
-        // if(root.right != null) invertTree(root.left);
-        // if(root.left != null) invertTree(root.right);
-        // return root;
-        // above code is written by me showing test cases has passed but hidden test cases has not passed
-
-        // by teacher sol;
         if(root == null) return root;
         TreeNode l = root.left;
         TreeNode r = root.right;
-        
         root.left = invertTree(r);
         root.right = invertTree(l);
         return root;
